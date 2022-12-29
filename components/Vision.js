94c4a64c780c9bf4d6a713fc1ed8ce01ec5f896d
Saksham Gupta <creator.sakshamgupta@gmail.com>
2022-12-29 22:01:54 +0530

    Initial Commit 4

--- components/Vision.js
+++ components/Vision.js
@@ -1,6 +1,5 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+
+import React from 'react';
 
 export default function About() {
     return (
@@ -24,10 +23,7 @@ export default function About() {
               </p>
 
             </div>
-            {/* <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-              <Image src={AboutImg} className='rounded-xl' alt='/' />
-            </div> */}
           </div>
         </div>
-      );
-};
+      )
+}
